d2852b610ed00256d452f592e4e227ec0f0782f5
jssouto <jorgesusouto@gmail.com>
2025-03-22 19:37:16 +0100

    creamos proyecto Spring Initializr

---
+++ backend/ioh/src/test/java/es/upm/dit/isst/ioh/IohApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.ioh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IohApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
