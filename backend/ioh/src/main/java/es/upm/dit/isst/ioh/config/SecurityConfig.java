870108ea7e0c4a8581e30b06361dcc4c0a41a03e
Pablodomcha <pablo.dominguez.chafer@alumnos.upm.es>
2025-05-03 00:37:30 +0200

    Spring Boot Security inicial creada

--- backend/ioh/src/main/java/es/upm/dit/isst/ioh/config/SecurityConfig.java
+++ backend/ioh/src/main/java/es/upm/dit/isst/ioh/config/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,13 +14,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .cors().and()
-            .csrf().disable()
-            .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll()
-            )
-            .headers(headers -> headers.frameOptions().disable());
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/**").permitAll(); // ultima linea, añadir todas las restricciones antes
+        })
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/**")) // Disable CSRF (unneeded for REST API)
+                .headers(headers -> headers.frameOptions().sameOrigin()); // Allow frames for H2 console
         return http.build();
     }
 
@@ -31,12 +27,10 @@ public class SecurityConfig {
         JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
         // Query to fetch user credentials
         userDetailsManager.setUsersByUsernameQuery(
-            "SELECT username, password, enabled FROM USUARIO WHERE username = ?"
-        );
+                "SELECT username, password, enabled FROM USUARIO WHERE username = ?");
         // Query to fetch user roles
         userDetailsManager.setAuthoritiesByUsernameQuery(
-            "SELECT username, authority AS authority FROM USUARIO WHERE username = ?"
-        );
+                "SELECT username, authority AS authority FROM USUARIO WHERE username = ?");
         return userDetailsManager;
     }
 }
